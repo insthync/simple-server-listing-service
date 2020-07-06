84056f467268964b3649058e0b643af409da4808
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2020-07-06 13:39:49 +0700

    Changes string and number to typescript' type

--- server_data.ts
+++ server_data.ts
@@ -1,11 +1,11 @@
 export class ServerData
 {
-    public id : String = "";
-    public address : String = "";
-    public port : Number = 0;
-    public title : String = "";
-    public description : String = "";
-    public map : String = "";
-    public currentPlayer : Number = 0;
-    public maxPlayer : Number = 0;
+    public id : string = "";
+    public address : string = "";
+    public port : number = 0;
+    public title : string = "";
+    public description : string = "";
+    public map : string = "";
+    public currentPlayer : number = 0;
+    public maxPlayer : number = 0;
 }
